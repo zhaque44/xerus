4e6867376d8c4669511ddd1c75348e169aa8b46c
zhaque44 <haque.zubair@gmail.com>
2023-03-29 10:59:37 -0500

    create decision tree for recommendations

---
+++ decisionanalysis/recommendations.go
@@ -0,0 +1,140 @@
+package decisionanalysis
+
+import (
+	"fmt"
+)
+
+type decisionNode struct {
+	question  string
+	yesBranch *decisionNode
+	noBranch  *decisionNode
+	result    string
+}
+
+func askQuestion(question string) bool {
+	var answer string
+	fmt.Print(question + " (y/n) ")
+	fmt.Scanln(&answer)
+	return answer == "y"
+}
+
+func makeDecisionTree(cost float64, leaseTerm int, interestRate float64, residualValue float64, maintenanceCost *float64, repairCost *float64, marketDemand *string, competitiveLandscape *string, taxImplications *string) *decisionNode {
+	rootNode := &decisionNode{
+		question: fmt.Sprintf("Is the cost of the asset greater than $%.2f?", cost),
+		yesBranch: &decisionNode{
+			question: fmt.Sprintf("Will the asset be leased for more than %d years?", leaseTerm),
+			yesBranch: &decisionNode{
+				question: fmt.Sprintf("Is the interest rate higher than %.2f%%?", interestRate*100),
+				yesBranch: &decisionNode{
+					question: "Are the maintenance and repair costs low?",
+					yesBranch: &decisionNode{
+						question: fmt.Sprintf("Will the residual value of the asset be more than %.2f%% of the initial cost?", residualValue*100),
+						yesBranch: &decisionNode{
+							result: "Leasing is recommended.",
+						},
+						noBranch: &decisionNode{
+							result: "Buying is recommended.",
+						},
+					},
+					noBranch: &decisionNode{
+						result: "Buying is recommended.",
+					},
+				},
+				noBranch: &decisionNode{
+					result: "Buying is recommended.",
+				},
+			},
+			noBranch: &decisionNode{
+				question: fmt.Sprintf("Will the residual value of the asset be more than %.2f%% of the initial cost?", residualValue*100),
+				yesBranch: &decisionNode{
+					result: "Leasing is recommended.",
+				},
+				noBranch: &decisionNode{
+					result: "Buying is recommended.",
+				},
+			},
+		},
+		noBranch: &decisionNode{
+			result: "Buying is recommended.",
+		},
+	}
+
+	if maintenanceCost != nil {
+		rootNode.yesBranch.yesBranch.noBranch.yesBranch.yesBranch = &decisionNode{
+			question: fmt.Sprintf("Are the total costs of ownership lower than leasing the asset for %d years?", leaseTerm),
+			yesBranch: &decisionNode{
+				result: "Buying is recommended.",
+			},
+			noBranch: &decisionNode{
+				result: "Leasing is recommended.",
+			},
+		}
+	}
+
+	if marketDemand != nil {
+		rootNode.yesBranch.yesBranch.noBranch.yesBranch.noBranch = &decisionNode{
+			question: fmt.Sprintf("Is the market demand for the asset high (%s)?", *marketDemand),
+			yesBranch: &decisionNode{
+				result: "Leasing is recommended.",
+			},
+			noBranch: &decisionNode{
+				result: "Buying is recommended.",
+			},
+		}
+	}
+
+	if competitiveLandscape != nil {
+		rootNode.yesBranch.yesBranch.noBranch.noBranch = &decisionNode{
+			question: fmt.Sprintf("Is the competitive landscape for the asset favorable to leasing (%s)?", *competitiveLandscape),
+			yesBranch: &decisionNode{
+				result: "Leasing is recommended.",
+			},
+			noBranch: &decisionNode{
+				result: "Buying is recommended.",
+			},
+		}
+	}
+
+	if taxImplications != nil {
+		rootNode.yesBranch.yesBranch.noBranch.yesBranch.noBranch = &decisionNode{
+			question: fmt.Sprintf("Are there tax implications that make one option more beneficial (%s)?", *taxImplications),
+			yesBranch: &decisionNode{
+				result: "Leasing is recommended.",
+			},
+			noBranch: &decisionNode{
+				result: "Buying is recommended.",
+			},
+		}
+	}
+
+	if repairCost != nil {
+		rootNode.yesBranch.yesBranch.noBranch.yesBranch.noBranch = &decisionNode{
+			question: fmt.Sprintf("Are the repair costs high (%.2f)?", *repairCost),
+			yesBranch: &decisionNode{
+				result: "Leasing is recommended.",
+			},
+			noBranch: &decisionNode{
+				result: "Buying is recommended.",
+			},
+		}
+	}
+
+	return rootNode
+}
+
+func getRecommendation(node *decisionNode) string {
+	if node.result != "" {
+		return node.result
+	}
+
+	if askQuestion(node.question) {
+		return getRecommendation(node.yesBranch)
+	} else {
+		return getRecommendation(node.noBranch)
+	}
+}
+
+func GetRecommendation(cost float64, leaseTerm int, interestRate float64, residualValue float64, maintenanceCost *float64, repairCost *float64, marketDemand *string, competitiveLandscape *string, taxImplications *string) string {
+	rootNode := makeDecisionTree(cost, leaseTerm, interestRate, residualValue, maintenanceCost, repairCost, marketDemand, competitiveLandscape, taxImplications)
+	return getRecommendation(rootNode)
+}
--- main.go
+++ main.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"fmt"
-	"math"
 
-	"xerus/trendanalysis"
+	"xerus/decisionanalysis"
 )
 
 func main() {
-	// Example usage of the MovingAverage and MovingAverageMissing functions
-	data := []float64{1, 2, math.NaN(), 4, 5, 6, 7, 8, 9, 10}
-	window := 3
-	smoothed, err := trendanalysis.SimpleMovingAverageMissing(data, window)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(smoothed)
+	// Test 1: Basic usage
+	// Inputs: 40000, 36, 0.03, 15000, nil, nil, nil, nil, nil
+	// Expected output: "Lease"
+	output := decisionanalysis.GetRecommendation(40000, 36, 0.03, 15000, nil, nil, nil, nil, nil)
+	fmt.Println(output)
+
+	// Test 2: With maintenance cost and repair cost
+	// Inputs: 35000, 48, 0.04, 10000, 500, 800, nil, nil, nil
+	// Expected output: "Buy"
+	// maintenanceCost := 500.0
+	// repairCost := 800.0
+	// output = decisionanalysis.GetRecommendation(35000, 48, 0.04, 10000, &maintenanceCost, &repairCost, nil, nil, nil)
+	// fmt.Printf("Test 2: %v\n", output == "Buy")
+
+	// Test 3: With market demand and competitive landscape
+	// Inputs: 30000, 24, 0.05, 8000, nil, nil, "High", "Very Competitive", nil
+	// Expected output: "Lease"
+	// marketDemand := "High"
+	// competitiveLandscape := "Very Competitive"
+	// output = decisionanalysis.GetRecommendation(30000, 24, 0.05, 8000, nil, nil, &marketDemand, &competitiveLandscape, nil)
+	// fmt.Printf("Test 3: %v\n", output == "Lease")
+
+	// Test 4: With tax implications
+	// Inputs: 45000, 36, 0.03, 20000, nil, nil, nil, nil, "Positive"
+	// Expected output: "Lease"
+	// taxImplications := "Positive"
+	// output = decisionanalysis.GetRecommendation(45000, 36, 0.03, 20000, nil, nil, nil, nil, &taxImplications)
+	// fmt.Printf("Test 4: %v\n", output == "Lease")
+
+	// Test 5: With all inputs
+	// Inputs: 40000, 48, 0.06, 12000, 1000, 1500, "Low", "Moderately Competitive", "Negative"
+	// Expected output: "Buy"
+	// maintenanceCost = 1000.0
+	// repairCost = 1500.0
+	// marketDemand = "Low"
+	// competitiveLandscape = "Moderately Competitive"
+	// taxImplications = "Negative"
+	// output = decisionanalysis.GetRecommendation(40000, 48, 0.06, 12000, &maintenanceCost, &repairCost, &marketDemand, &competitiveLandscape, &taxImplications)
+	// fmt.Printf("Test 5: %v\n", output == "Buy")
 }
