37c4ac118316bee1044c226b129acd57971633ae
zhaque44 <haque.zubair@gmail.com>
2023-03-29 23:49:30 -0500

    i

--- decisionanalysis/customer_classification.go
+++ decisionanalysis/customer_classification.go
@@ -1,56 +1,34 @@
 package decisionanalysis
 
-import (
-	"regexp"
+import "time"
 
-	"github.com/texttheater/golang-levenshtein/levenshtein"
-)
-
-// Customer struct to represent a customer
 type Customer struct {
-	ID     int
-	Name   string
-	Email  string
-	Churn  bool
-	Orders int
+	ID           int
+	Name         string
+	Email        string
+	Churn        bool
+	OrderHistory []Order
 }
 
-// Dataset struct to represent a dataset of customers
+type Order struct {
+	ID          int
+	CustomerID  int
+	OrderDate   time.Time
+	TotalAmount float64
+}
+
 type Dataset struct {
 	Customers []Customer
 }
 
-// findPattern searches for a regular expression pattern in a 2D slice of strings
-// and returns a 2D slice of matched strings
-func findPattern(strings [][]string, pattern string) [][]string {
-	matches := [][]string{}
-	for _, row := range strings {
-		for _, str := range row {
-			if ok, _ := regexp.MatchString(pattern, str); ok {
-				matches = append(matches, []string{str})
-			}
-		}
-	}
-	return matches
-}
-
-// classifyChurn uses the findPattern function and a classification algorithm to predict
-// whether a customer is likely to churn based on their email address
-func ClassifyChurn(customer Customer) bool {
-	// Search for a pattern in the email address
-	pattern := "gmail"
-	matches := findPattern([][]string{{customer.Email}}, pattern)
-
-	// Calculate the Levenshtein distance between the email and the pattern
-	var dist int
-	if len(matches) > 0 {
-		dist = levenshtein.DistanceForStrings([]rune(customer.Email), []rune(matches[0][0]), levenshtein.DefaultOptions)
-	} else {
-		dist = levenshtein.DistanceForStrings([]rune(customer.Email), []rune(pattern), levenshtein.DefaultOptions)
+func predictChurn(customer Customer, churnThresholdAmount float64, churnThresholdMonths int) bool {
+	var totalAmount float64
+	for _, order := range customer.OrderHistory {
+		totalAmount += order.TotalAmount
 	}
 
-	// If the distance is less than or equal to half the length of the pattern, predict churn
-	if dist <= len(pattern)/2 {
+	// If the total order amount is less than the churn threshold amount or there hasn't been an order in the last churn threshold months, predict churn
+	if totalAmount < churnThresholdAmount || time.Since(customer.OrderHistory[len(customer.OrderHistory)-1].OrderDate).Hours()/24/30 > float64(churnThresholdMonths) {
 		return true
 	} else {
 		return false
--- decisionanalysis/customer_classification_test.go
+++ decisionanalysis/customer_classification_test.go
@@ -3,40 +3,75 @@ package decisionanalysis
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestClassifyChurn(t *testing.T) {
 	dataset := Dataset{
 		Customers: []Customer{
 			{
-				ID:     1,
-				Name:   "John Doe",
-				Email:  "johndoe@gmail.com",
-				Churn:  false,
-				Orders: 10,
+				ID:    1,
+				Name:  "John Doe",
+				Email: "johndoe@gmail.com",
+				Churn: true,
+				OrderHistory: []Order{
+					{
+						ID:          1,
+						CustomerID:  1,
+						OrderDate:   time.Now().AddDate(0, -9, 0),
+						TotalAmount: 50.0,
+					},
+					{
+						ID:          2,
+						CustomerID:  1,
+						OrderDate:   time.Now().AddDate(0, -10, 0),
+						TotalAmount: 75.0,
+					},
+				},
 			},
 			{
-				ID:     2,
-				Name:   "Jane Smith",
-				Email:  "janesmith@yahoo.com",
-				Churn:  true,
-				Orders: 5,
+				ID:    2,
+				Name:  "Jane Smith",
+				Email: "janesmith@hotmail.com",
+				Churn: false,
+				OrderHistory: []Order{
+					{
+						ID:          1,
+						CustomerID:  2,
+						OrderDate:   time.Now().AddDate(0, -1, 0),
+						TotalAmount: 125.0,
+					},
+					{
+						ID:          2,
+						CustomerID:  2,
+						OrderDate:   time.Now().AddDate(0, -3, 0),
+						TotalAmount: 500.0,
+					},
+				},
 			},
 			{
-				ID:     3,
-				Name:   "Bob Johnson",
-				Email:  "bobjohnson@gmail.com",
-				Churn:  false,
-				Orders: 8,
+				ID:    3,
+				Name:  "Bob Jones",
+				Email: "bobjones@gmail.com",
+				Churn: true,
+				OrderHistory: []Order{
+					{
+						ID:          1,
+						CustomerID:  3,
+						OrderDate:   time.Now().AddDate(0, -7, 0),
+						TotalAmount: 125.0,
+					},
+				},
 			},
 		},
 	}
 
+	// Predict churn for each customer in the dataset
 	for _, customer := range dataset.Customers {
-		if ClassifyChurn(customer) {
-			fmt.Printf("Customer %d is likely to churn\n", customer.ID)
+		if predictChurn(customer, 200.0, 12) {
+			fmt.Printf("%s is likely to churn\n", customer.Name)
 		} else {
-			fmt.Printf("Customer %d is not likely to churn\n", customer.ID)
+			fmt.Printf("%s is not likely to churn\n", customer.Name)
 		}
 	}
 }
