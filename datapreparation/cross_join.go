57eece3ea60a4de09ed3af0f71785f0f75ca73ac
Zubair Haque <haque.zubair@gmail.com>
2023-08-17 18:03:04 -0500

    Update cross_join.go with imports

--- datapreparation/cross_join.go
+++ datapreparation/cross_join.go
@@ -1,64 +1,62 @@
+package datapreparation
 
+import (
+	"fmt"
 
+	"github.com/go-gota/gota/dataframe"
+	"github.com/pkg/errors"
+)
 
-func LeftJoin(left DataFrame, right DataFrame, onColumn string) (DataFrame, error) {
-    // Check if the onColumn exists in both DataFrames
-    if !left.HasCol(onColumn) || !right.HasCol(onColumn) {
-        return DataFrame{}, fmt.Errorf("specified onColumn '%s' not found in both DataFrames", onColumn)
-    }
-    
-    // Find the indices of the onColumn in both DataFrames
-    leftIndex := left.ColIndex(onColumn)
-    rightIndex := right.ColIndex(onColumn)
+func LeftJoin(left dataframe.DataFrame, right dataframe.DataFrame, onColumn string) (dataframe.DataFrame, error) {
+	if !left.HasCol(onColumn) || !right.HasCol(onColumn) {
+		return dataframe.DataFrame{}, errors.Errorf("specified onColumn '%s' not found in both DataFrames", onColumn)
+	}
 
-    // Initialize the resulting DataFrame
-    var resultCols []series.Series
-    for _, col := range left.Columns() {
-        resultCols = append(resultCols, col.Empty())
-    }
-    for _, col := range right.Columns() {
-        if col.Name() != onColumn {
-            resultCols = append(resultCols, col.Empty())
-        }
-    }
+	leftIndex := left.ColIndex(onColumn)
+	rightIndex := right.ColIndex(onColumn)
 
-    // Iterate through the left DataFrame and perform the left join
-    for i := 0; i < left.Nrow(); i++ {
-        leftValue := left.Elem(leftIndex, i)
-        found := false
+	var resultCols []dataframe.Series
+	for _, col := range left.Columns() {
+		resultCols = append(resultCols, col.Empty())
+	}
+	for _, col := range right.Columns() {
+		if col.Name() != onColumn {
+			resultCols = append(resultCols, col.Empty())
+		}
+	}
 
-        // Search for matching rows in the right DataFrame
-        for j := 0; j < right.Nrow(); j++ {
-            rightValue := right.Elem(rightIndex, j)
-            if leftValue == rightValue {
-                found = true
+	for i := 0; i < left.Nrow(); i++ {
+		leftValue := left.Elem(leftIndex, i)
+		found := false
 
-                // Append values from the left DataFrame
-                for _, col := range left.Columns() {
-                    resultCols[col.Index()].Append(col.Elem(i))
-                }
+		for j := 0; j < right.Nrow(); j++ {
+			rightValue := right.Elem(rightIndex, j)
+			if leftValue == rightValue {
+				found = true
 
-                // Append values from the right DataFrame (excluding the onColumn)
-                for _, col := range right.Columns() {
-                    if col.Name() != onColumn {
-                        resultCols[col.Index()+left.Ncol()].Append(col.Elem(j))
-                    }
-                }
-            }
-        }
+				for _, col := range left.Columns() {
+					resultCols[col.Index()].Append(col.Elem(i))
+				}
 
-        // If no match found, append null values for right DataFrame columns
-        if !found {
-            for _, col := range left.Columns() {
-                resultCols[col.Index()].Append(col.Elem(i))
-            }
-            for _, col := range right.Columns() {
-                if col.Name() != onColumn {
-                    resultCols[col.Index()+left.Ncol()].Append(nil)
-                }
-            }
-        }
-    }
+				for _, col := range right.Columns() {
+					if col.Name() != onColumn {
+						resultCols[col.Index()+left.Ncol()].Append(col.Elem(j))
+					}
+				}
+			}
+		}
 
-    return New(resultCols...), nil
+		if !found {
+			for _, col := range left.Columns() {
+				resultCols[col.Index()].Append(col.Elem(i))
+			}
+			for _, col := range right.Columns() {
+				if col.Name() != onColumn {
+					resultCols[col.Index()+left.Ncol()].Append(nil)
+				}
+			}
+		}
+	}
+
+	return dataframe.New(resultCols...), nil
 }
