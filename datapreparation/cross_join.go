5b3105558cffcb85d9ce7a5095b5afbecdd1993b
Zubair Haque <haque.zubair@gmail.com>
2023-08-18 12:04:59 -0200

    Update cross_join.go

--- datapreparation/cross_join.go
+++ datapreparation/cross_join.go
@@ -11,24 +11,57 @@ func LeftJoin(left dataframe.DataFrame, right dataframe.DataFrame, onColumn stri
 		return dataframe.DataFrame{}, errors.Errorf("specified onColumn '%s' not found in both DataFrames", onColumn)
 	}
 
-	// Perform cross join to get all combinations
-	crossJoined := left.CrossJoin(right)
-
-	// Filter rows to mimic left join behavior
-	filteredRows := make([]int, 0)
 	leftIndex := left.ColIndex(onColumn)
 	rightIndex := right.ColIndex(onColumn)
 
-	for i := 0; i < crossJoined.Nrow(); i++ {
-		leftValue := crossJoined.Elem(leftIndex, i)
-		rightValue := crossJoined.Elem(rightIndex + left.Ncol(), i)
+	// Check if the join columns have the same data type
+	if left.ColTypes()[leftIndex] != right.ColTypes()[rightIndex] {
+		return dataframe.DataFrame{}, errors.New("join columns have different data types")
+	}
 
-		if leftValue == rightValue {
-			filteredRows = append(filteredRows, i)
+	var resultCols []dataframe.Series
+	for _, col := range left.Columns() {
+		resultCols = append(resultCols, col.Empty())
+	}
+	for _, col := range right.Columns() {
+		if col.Name() != onColumn {
+			resultCols = append(resultCols, col.Empty())
 		}
 	}
 
-	filteredDF := crossJoined.Subset(filteredRows)
+	for i := 0; i < left.Nrow(); i++ {
+		leftValue := left.Elem(leftIndex, i)
+		found := false
 
-	return filteredDF, nil
+		for j := 0; j < right.Nrow(); j++ {
+			rightValue := right.Elem(rightIndex, j)
+			if leftValue == rightValue {
+				found = true
+
+				for _, col := range left.Columns() {
+					resultCols[col.Index()].Append(col.Elem(i))
+				}
+
+				for _, col := range right.Columns() {
+					if col.Name() != onColumn {
+						resultCols[col.Index()+left.Ncol()].Append(col.Elem(j))
+					}
+				}
+			}
+		}
+
+		if !found {
+			for _, col := range left.Columns() {
+				resultCols[col.Index()].Append(col.Elem(i))
+			}
+			for _, col := range right.Columns() {
+				if col.Name() != onColumn {
+					resultCols[col.Index()+left.Ncol()].Append(nil)
+				}
+			}
+		}
+	}
+
+	return dataframe.New(resultCols...), nil
 }
+	
