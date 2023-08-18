b0c4e78044459b5df1d7bd8f4ca4995fbb4d9d26
Zubair Haque <haque.zubair@gmail.com>
2023-08-18 11:58:29 -0200

    Update cross_join.go

--- datapreparation/cross_join.go
+++ datapreparation/cross_join.go
@@ -2,7 +2,6 @@ package datapreparation
 
 import (
 	"fmt"
-
 	"github.com/go-gota/gota/dataframe"
 	"github.com/pkg/errors"
 )
@@ -12,51 +11,24 @@ func LeftJoin(left dataframe.DataFrame, right dataframe.DataFrame, onColumn stri
 		return dataframe.DataFrame{}, errors.Errorf("specified onColumn '%s' not found in both DataFrames", onColumn)
 	}
 
+	// Perform cross join to get all combinations
+	crossJoined := left.CrossJoin(right)
+
+	// Filter rows to mimic left join behavior
+	filteredRows := make([]int, 0)
 	leftIndex := left.ColIndex(onColumn)
 	rightIndex := right.ColIndex(onColumn)
 
-	var resultCols []dataframe.Series
-	for _, col := range left.Columns() {
-		resultCols = append(resultCols, col.Empty())
-	}
-	for _, col := range right.Columns() {
-		if col.Name() != onColumn {
-			resultCols = append(resultCols, col.Empty())
+	for i := 0; i < crossJoined.Nrow(); i++ {
+		leftValue := crossJoined.Elem(leftIndex, i)
+		rightValue := crossJoined.Elem(rightIndex + left.Ncol(), i)
+
+		if leftValue == rightValue {
+			filteredRows = append(filteredRows, i)
 		}
 	}
 
-	for i := 0; i < left.Nrow(); i++ {
-		leftValue := left.Elem(leftIndex, i)
-		found := false
+	filteredDF := crossJoined.Subset(filteredRows)
 
-		for j := 0; j < right.Nrow(); j++ {
-			rightValue := right.Elem(rightIndex, j)
-			if leftValue == rightValue {
-				found = true
-
-				for _, col := range left.Columns() {
-					resultCols[col.Index()].Append(col.Elem(i))
-				}
-
-				for _, col := range right.Columns() {
-					if col.Name() != onColumn {
-						resultCols[col.Index()+left.Ncol()].Append(col.Elem(j))
-					}
-				}
-			}
-		}
-
-		if !found {
-			for _, col := range left.Columns() {
-				resultCols[col.Index()].Append(col.Elem(i))
-			}
-			for _, col := range right.Columns() {
-				if col.Name() != onColumn {
-					resultCols[col.Index()+left.Ncol()].Append(nil)
-				}
-			}
-		}
-	}
-
-	return dataframe.New(resultCols...), nil
+	return filteredDF, nil
 }
