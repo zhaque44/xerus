2e3470a3de694b406e1c35675a79e1f641390679
Zubair Haque <haque.zubair@gmail.com>
2023-08-18 12:16:03 -0200

    Update cross_join.go

--- datapreparation/cross_join.go
+++ datapreparation/cross_join.go
@@ -7,7 +7,6 @@ import (
 )
 
 func LeftJoin(left dataframe.DataFrame, right dataframe.DataFrame, onColumn string) (dataframe.DataFrame, error) {
-	// Check if the join column exists in both DataFrames
 	if !left.HasCol(onColumn) || !right.HasCol(onColumn) {
 		return dataframe.DataFrame{}, errors.Errorf("specified onColumn '%s' not found in both DataFrames", onColumn)
 	}
@@ -15,18 +14,13 @@ func LeftJoin(left dataframe.DataFrame, right dataframe.DataFrame, onColumn stri
 	leftIndex := left.ColIndex(onColumn)
 	rightIndex := right.ColIndex(onColumn)
 
-	// Check if the join columns have the same data type
 	if left.ColTypes()[leftIndex] != right.ColTypes()[rightIndex] {
 		return dataframe.DataFrame{}, errors.New("join columns have different data types")
 	}
 
-	// Perform cross join to create the initial cross-joined DataFrame
 	crossJoined := left.CrossJoin(right)
-
-	// Initialize a slice to store indices of rows that match the join condition
 	matchingRows := make([]int, 0)
 
-	// Filter rows based on matching join condition
 	for i := 0; i < crossJoined.Nrow(); i++ {
 		leftValue := crossJoined.Elem(leftIndex, i)
 		rightValue := crossJoined.Elem(rightIndex + left.Ncol(), i)
@@ -36,8 +30,6 @@ func LeftJoin(left dataframe.DataFrame, right dataframe.DataFrame, onColumn stri
 		}
 	}
 
-	// Subset the DataFrame with the matching rows
 	filteredDF := crossJoined.Subset(matchingRows)
-
 	return filteredDF, nil
 }
