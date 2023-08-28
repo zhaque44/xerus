9c5326545865b8caa65efa7998edec55aa8382b6
zhaque44 <haque.zubair@gmail.com>
2023-08-28 09:45:41 -0500

    adding tests with assertions

--- collections/intersection_test.go
+++ collections/intersection_test.go
@@ -1,27 +1,33 @@
 package collections
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIntersection(t *testing.T) {
+	assert := assert.New(t)
+
 	// Example usage with slices of different types
 	intSlice1 := []int{1, 2, 3, 4, 5}
 	intSlice2 := []int{4, 5, 6, 7, 8}
-	intersection1, _ := intersection(intSlice1, intSlice2)
-	fmt.Println("Intersection of int slices:", intersection1)
+	intersection1, err := intersection(intSlice1, intSlice2)
+	assert.NoError(err)
+	assert.ElementsMatch(intersection1, []int{4, 5})
 
 	floatSlice1 := []float64{1.1, 2.2, 3.3, 4.4, 5.5}
 	floatSlice2 := []float64{4.4, 5.5, 6.6, 7.7, 8.8}
-	intersection2, _ := intersection(floatSlice1, floatSlice2)
-	fmt.Println("Intersection of float slices:", intersection2)
+	intersection2, err := intersection(floatSlice1, floatSlice2)
+	assert.NoError(err)
+	assert.ElementsMatch(intersection2, []float64{4.4, 5.5})
 
 	stringSlice1 := []string{"apple", "banana", "cherry", "date"}
 	stringSlice2 := []string{"cherry", "date", "elderberry", "fig"}
-	intersection3, _ := intersection(stringSlice1, stringSlice2)
-	fmt.Println("Intersection of string slices:", intersection3)
+	intersection3, err := intersection(stringSlice1, stringSlice2)
+	assert.NoError(err)
+	assert.ElementsMatch(intersection3, []string{"cherry", "date"})
 
 	// Generate a sample dataset of custom type
 	type DataPoint struct {
@@ -40,11 +46,6 @@ func TestIntersection(t *testing.T) {
 
 	// Ensure there are no common data points between the two sets
 	intersection4, err := intersection(training, testing)
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else if len(intersection4) > 0 {
-		fmt.Println("Error: training and testing sets share data points")
-	} else {
-		fmt.Println("Training and testing sets do not share data points")
-	}
+	assert.NoError(err)
+	assert.Empty(intersection4, "Training and testing sets should not share data points")
 }
