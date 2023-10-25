53d06b732c3a180dbca1de98f25613ff93193fd9
zhaque44 <haque.zubair@gmail.com>
2023-10-24 21:21:37 -0500

    iii

--- fileops/fileops.go
+++ fileops/fileops.go
@@ -2,7 +2,9 @@ package fileops
 
 import (
 	"bufio"
+	"errors"
 	"os"
+	"path/filepath"
 )
 
 func LoadFile(filepath string) ([]string, error) {
@@ -36,4 +38,17 @@ func IsPipedInput() (bool, error) {
 	isCharDevice := (mode & os.ModeCharDevice) == 0
 
 	return isCharDevice || isFIFO, nil
+}
+
+func FindFilesByPattern(pattern string) ([]string, error) {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(matches) == 0 {
+		return nil, errors.New("no files found matching the pattern")
+	}
+
+	return matches, nil
 }
